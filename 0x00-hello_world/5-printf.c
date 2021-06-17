3ae0f4d885b18b8cbfd777e0a17635a77933c2cc
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-06-17 10:33:30 -0700

    Answer for the six question

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prinf: print the characters given
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,/m");
+	return (0);
+}
