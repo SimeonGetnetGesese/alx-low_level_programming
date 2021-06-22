7cca3f39193662422724407890f3ae63bfa7ae02
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-06-22 11:05:30 -0700

    Answer to 2nd question

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,8 @@
  * Print's a-z small letters
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+
+void print_alphabet(void);
 {
 	int c;
 
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,5 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
 
 int _putchar(char c);
 void print_alphabet(void);
 
 
-#endif
