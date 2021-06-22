bfa80208f295a87db906d8227552eb57187d9904
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-06-22 10:58:08 -0700

    Adjusted answer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
+#include "holberton.h"
 
 
 /**
  * main - assigns a random number to int n
- * Print's Holberton
+ * Print's a-z small letters
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-void print_alphabet(void)
-{
-	char c;
+	int c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
