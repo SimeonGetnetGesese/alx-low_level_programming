f0f03d4fa2c46e70dcb795b7f342c50bf2b813bb
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-06-22 10:46:59 -0700

    Revised answer for 1&2 question

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - assigns a random number to int n
+ * Print's Holberton
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
