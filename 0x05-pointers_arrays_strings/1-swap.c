3ed4b9a49dc64fac663765cff9f01ef3190b9a9b
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-06-28 10:17:45 -0700

    Answer for the 2nd question

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swap the value of a and b.
+ * @a: Change value of a to 42.
+ * @b: Changes value of b to 98.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ * @a: Change value of a to 42.
+ * @b: Changes value of b to 98.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
