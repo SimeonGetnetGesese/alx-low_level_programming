6e66428246dcdc022d46152ce79e480cb63255b6
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-07-12 08:10:17 -0700

    Answer to Mandatory Question

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number os strings with out the comand.
+ * @argc: argument count.
+ * @argv: argument variable.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
