04da0a7d0ab529379b0eb247d5ca7c082854c74d
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-07-13 08:13:29 -0700

    Answer to Mandatory Question.

---
+++ 0x0B-malloc_free/100-main.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * @ac: is anchor size.
+ * @av: is anchor values.
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+	char *s;
+
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
