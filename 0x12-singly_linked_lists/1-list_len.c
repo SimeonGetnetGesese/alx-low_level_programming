02a3c8c1a81a018f7be14550dd49aa0ca311e692
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-07-29 12:30:15 -0700

    Answer to mandatory Questions.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,42 +1,16 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
  */
-int _strlen(char *s)
+size_t list_len(const list_t *h)
 {
-	char *p = s;
+	int i;
 
-	while (*s)
-		s++;
-	return (s - p);
-}
-/**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: reference to head of linked list
- * @str: string to be added on new node
- * Return: address of new head;
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *newNode;
-	char *newStr;
-
-	if (!str)
-		return (NULL);
-	newNode = malloc(sizeof(list_t));
-	if (!newNode)
-		return (NULL);
-	newStr = strdup(str);
-	if (!newStr)
-	{
-		free(newNode);
-		return (NULL);
-	}
-	newNode->len = _strlen(newStr);
-	newNode->str = newStr;
-	newNode->next = *head;
-	*head = newNode;
-	return (*head);
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
 }
